5827526aa4c51e962fac58471f18b1ca661b547e
Charel Felten <charel.felten@gmail.com>
2021-07-02 10:02:27 +0200

    starting work on dates

--- Kiwi/SettingsView.swift
+++ Kiwi/SettingsView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State var nightBreak: Bool = true
+    @State var nightBreakStart = Date()
+    
     var body: some View {
         List {
-            Text("Settings")
-        }
+            Section(header: Text("General")) {
+                Toggle(isOn: $nightBreak.animation()) {
+                    Text("Sleep mode")
+                }
+                
+                if nightBreak {
+                    HStack {
+                        DatePicker("from", selection: $nightBreakStart, displayedComponents: .hourAndMinute)
+                        DatePicker("until", selection: $nightBreakStart, displayedComponents: .hourAndMinute)
+                    }
+                    
+                }
+            }
+            
+            Section(header: Text("High Priority")) {
+                
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+        }.listStyle(GroupedListStyle())
     }
 }
 
