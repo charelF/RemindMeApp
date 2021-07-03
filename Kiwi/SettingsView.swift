7e8aed0fce6e48e8cc47424e90490451b8075c8c
Charel Felten <charel.felten@gmail.com>
2021-07-03 14:50:31 +0200

    swiftui ticker is greyed out

--- Kiwi/SettingsView.swift
+++ Kiwi/SettingsView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 
+enum Interval: String, Equatable, CaseIterable {
+    case month = "Month"
+    case week = "Week"
+    case day = "Day"
+    case hour = "Hour"
+    case minute = "Minute"
+}
+
 struct SettingsView: View {
     
     @State var nightBreak: Bool = true
+    
+    @State var priority0: Bool = true
+    @State var priority1: Bool = true
+    @State var priority2: Bool = true
+    @State var priority3: Bool = true
+    
     @State var nightBreakStart = Date()
+    @State var interval: Interval = .day
+    @State var amount: Int = 2
     
     var body: some View {
         List {
@@ -28,12 +44,37 @@ struct SettingsView: View {
                 }
             }
             
-            Section(header: Text("High Priority")) {
-                
-                Text("1")
-                Text("2")
-                Text("3")
+            Section(header: Text("No notification")) {
+                Toggle(isOn: $priority0) {
+                    Text("No notification notes")
+                }
             }
+            
+            Section(header: Text("Low Priority")) {
+                Toggle(isOn: $priority1.animation()) {
+                    Text("Low priority notes")
+                }
+                
+                if priority1 {
+                    
+                    Stepper(value: $amount, in: 1...30) {
+                        Text("Number of reminders")
+                    }
+
+                    
+                    
+                    Picker(selection: $interval, label: Text("per")) {
+                        ForEach(Interval.allCases, id: \.self) { value in
+                            Text(value.rawValue).tag(value)
+                        }
+                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    DatePicker("First reminder at", selection: $nightBreakStart, displayedComponents: .hourAndMinute)
+                    
+                }
+            }
+            
         }.listStyle(GroupedListStyle())
     }
 }
