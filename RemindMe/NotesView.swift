46b0185981373907f2927b7f09b2d42aa6c3e56a
Charel Felten <charel.felten@gmail.com>
2022-11-17 20:07:58 +0100

    refactored some code - one note view - notes view

--- RemindMe/NotesView.swift
+++ RemindMe/NotesView.swift
@@ -52,125 +52,141 @@ struct NotesView: View {
     editNote = nil
   }
   
-//  func editCell(
-//    noteContent: Binding<String>,
-//    submitFunc: @escaping () -> (),
-//    focus: FocusState<Bool>.Binding
-//  ) -> some View {
-//    return HStack {
-//      TextField("New Note", text: noteContent)
-//      .onSubmit(submitFunc)
-//      .focused(focus)
-//      if (!noteContent.isEmpty) {
-//        Button(action: submitFunc) {
-//          Image(systemName: "checkmark")
-//        }
-//      }
-//    }
-//  }
+  func createCustomReminder(_ note: Note) {
+    customDateNote = note
+    showCustomDateSheet = true
+  }
+  
+  func switchToEditNote(_ note: Note) {
+    editNote = note
+    editNoteContent = note.content ?? ""
+    editNoteIsFocused = true
+  }
   
   var body: some View {
     List {
       ForEach(notes) { note in
         if (note != editNote) {
           OneNoteView(config: config, note: note)
-          
-          // context menu for the note, cant be extracted because it works with both editNote and customDateNote
-          .contextMenu {
-            // bug in ios15: context menu may show outdated information
-            VStack {
-              Label("Created on: \(note.timestamp ?? Date(), formatter: Note.dateFormatter)", systemImage: "calendar")
-              Label("Reminders: \(note.priority.getIntervalDescription())", systemImage: "bell")
-              Button {
-                customDateNote = note
-                showCustomDateSheet = true
-              } label: {
-                Label("Create Custom Reminder", systemImage: "calendar.badge.plus")
+            .contextMenu {
+              NoteContextMenu(
+                note: note,
+                createCustomReminder: createCustomReminder,
+                switchToEditNote: switchToEditNote
+              )
+            }
+            .listRowBackground(
+              ZStack {
+                colorScheme == .dark ? Color.black : Color.white
+                note.getBackgroundColor()
               }
-              Button {
-                editNote = note
-                editNoteContent = note.content ?? newNoteContent
-                editNoteIsFocused = true
+            )
+            .swipeActions(edge: .trailing) {
+              Button(role: .destructive) {
+                note.delete()
               } label: {
-                Label("Edit Note", systemImage: "pencil")
+                Label("Delete", systemImage: "trash")
               }
             }
-          }
-          
-          // the background. We could add a backgroudn to the note view,
-          // but then it does not fill the whole row
-          // we use a ZStack to get rid of the ugly default background
-          // has to come after the context menu for some reason
-          .listRowBackground(
-            ZStack {
-              colorScheme == .dark ? Color.black : Color.white
-              note.getBackgroundColor()
-            }
+        } else {
+          EditNoteView(
+            noteContent: $editNoteContent,
+            isNoteFocused: _editNoteIsFocused,
+            onsubmit: {editExistingNote(note: editNote)},
+            placeholder: editNoteContent
           )
-          
-          // swipe actions (cant be extracted, because they need to be in a list)
-          .swipeActions(edge: .trailing) {
-            Button(role: .destructive) {
-              note.delete()
-            } label: {
-              Label("Delete", systemImage: "trash")
-            }
-          }
-        
-          
-        } else { // in the case the note is to be edited
-          HStack {
-            TextField(editNoteContent, text: $editNoteContent)
-            .onSubmit({editExistingNote(note: editNote)})
-            .focused($editNoteIsFocused)
-            if (!editNoteContent.isEmpty) {
-              Button(action: {editExistingNote(note: editNote)}) {
-                Image(systemName: "checkmark")
-              }
-            }
-          }
-        } // close edit note if-else
-      } // close for-each note
-      
+        }
+      }
       if editNote == nil {
-        HStack {
-          TextField("New Note", text: $newNoteContent)
-          .onSubmit(submitNewNote)
-          .focused($newNoteIsFocused)
-          if (!newNoteContent.isEmpty) {
-            Button(action: submitNewNote) {
-              Image(systemName: "checkmark")
-            }
-          }
-        }
-      } // close if of showing new note cell
-    } // close list
-    
-    .listStyle(InsetGroupedListStyle())
-    
-    .sheet(isPresented: $showCustomDateSheet) {
-      NavigationView{
-        VStack {
-          DatePicker("Reminder on", selection: $customDate)
-        }
-        .padding()
-        .navigationBarItems(leading: Button(action: {
-          showCustomDateSheet.toggle()
-        }) {
-          Text("Cancel")
-        }, trailing: Button(action: {
-          if let note = customDateNote {
-            note.updatePriority(optionalDate: customDate)
-          }
-          customDateNote = nil
-          showCustomDateSheet.toggle()
-        }) {
-          Text("Add")
-        }
+        EditNoteView(
+          noteContent: $newNoteContent,
+          isNoteFocused: _newNoteIsFocused,
+          onsubmit: submitNewNote,
+          placeholder: "New Note"
         )
       }
-    } // close custom note sheet
-  } // close body
+    }
+    .listStyle(InsetGroupedListStyle())
+    .sheet(isPresented: $showCustomDateSheet) {
+      CustomDateSheet(
+        customDateNote: $customDateNote,
+        showCustomDateSheet: $showCustomDateSheet,
+        customDate: $customDate
+      )
+    }
+  }
+}
+
+struct EditNoteView: View {
+  @Binding var noteContent: String
+  @FocusState var isNoteFocused: Bool
+  var onsubmit: () -> ()
+  var placeholder: String
+  
+  var body: some View {
+    HStack {
+      TextField(placeholder, text: $noteContent)
+        .onSubmit(onsubmit)
+        .focused($isNoteFocused)
+      if (!noteContent.isEmpty) {
+        Button(action: onsubmit) {
+          Image(systemName: "checkmark")
+        }
+      }
+    }
+  }
+}
+
+struct NoteContextMenu: View {
+  var note: Note
+  var createCustomReminder: (Note) -> ()
+  var switchToEditNote: (Note) -> ()
+
+  var body: some View {
+    VStack {
+      Label("Created on: \(note.timestamp ?? Date(), formatter: Note.dateFormatter)", systemImage: "calendar")
+      Label("Reminders: \(note.priority.getIntervalDescription())", systemImage: "bell")
+      Button(
+        action: { createCustomReminder(note) },
+        label: { Label("Create Custom Reminder", systemImage: "calendar.badge.plus") }
+      )
+      Button(
+        action: { switchToEditNote(note) },
+        label: { Label("Edit Note", systemImage: "pencil") }
+      )
+    }
+  }
+}
+
+struct CustomDateSheet: View {
+  @Binding var customDateNote: Note?
+  @Binding var showCustomDateSheet: Bool
+  @Binding var customDate: Date
+  
+  func cancelButtonAction() {
+    showCustomDateSheet.toggle()
+  }
+  
+  func addButtonAction() {
+    if let note = customDateNote {
+      note.updatePriority(optionalDate: customDate)
+    }
+    customDateNote = nil
+    showCustomDateSheet.toggle()
+  }
+  
+  var body: some View {
+    NavigationView{
+      VStack {
+        DatePicker("Reminder on", selection: $customDate)
+      }
+      .padding()
+      .navigationBarItems(
+        leading: Button("Cancel", action: cancelButtonAction),
+        trailing: Button("Add", action: addButtonAction)
+      )
+    }
+  }
 }
 
 struct NotesView_Previews: PreviewProvider {
--- RemindMe/OneNoteView.swift
+++ RemindMe/OneNoteView.swift
@@ -8,62 +8,67 @@
 import SwiftUI
 
 struct OneNoteView: View {
-    
-    @ObservedObject var config: Config
-    
-    // on having a childview update when parent changes:
-    // - https://stackoverflow.com/q/57614564/9439097
-    // - https://developer.apple.com/forums/thread/123920
-    @ObservedObject var note: Note
-    
-    @Environment(\.colorScheme) var colorScheme
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Text("\(note.content ?? "")")
-                    .padding(.vertical, 0.2)
-                    .fixedSize(horizontal: false, vertical: true)
-                Spacer()
-            }
-            
-            if (config.showCreationTime || config.showNotificationTime) {
-                HStack {
-                    if (config.showCreationTime) {
-                        Image(systemName: "calendar")
-                        Text("\(note.timestamp ?? Date.now, formatter: Note.dateFormatter)")
-                    }
-                    if (config.showNotificationTime || note.priority.isCustom()) {
-                        Image(systemName: "bell")
-                        Text(note.priority.getIntervalDescription())
-                    }
-                    Spacer()
-                }
-                .font(.footnote)
-                .foregroundColor(note.getSecondaryColor())
-                .padding(.bottom, 0.2)
-            }
-        }
-        
-        // to make the entire area clickable
-        .contentShape(Rectangle())
-        
-        // changing the foreground color
-        .foregroundColor(note.getPrimaryColor())
-        
-        // if we tap once on the note, we update its priority
-        .onTapGesture {
-            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-            note.updatePriority(optionalDate: nil)
-        }
+  // on having a childview update when parent changes:
+  // - https://stackoverflow.com/q/57614564/9439097
+  // - https://developer.apple.com/forums/thread/123920
+  
+  @ObservedObject var config: Config
+  @ObservedObject var note: Note
+  @Environment(\.colorScheme) var colorScheme
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      HStack {
+        Text("\(note.content ?? "")")
+          .padding(.vertical, 0.2)
+          .fixedSize(horizontal: false, vertical: true)
+        Spacer()
+      }
+      DetailView(config: config, note: note)
     }
+    // to make the entire area clickable
+    .contentShape(Rectangle())
+    // changing the foreground color
+    .foregroundColor(note.getPrimaryColor())
+    // if we tap once on the note, we update its priority
+    .onTapGesture {
+      UIImpactFeedbackGenerator(style: .light).impactOccurred()
+      note.updatePriority(optionalDate: nil)
+    }
+  }
+}
+
+struct DetailView: View {
+  // why @OO instead of @Binding? Idk, but maybe this helps
+  // - https://jaredsinclair.com/2020/05/07/swiftui-cheat-sheet.html
+  
+  @ObservedObject var config: Config
+  @ObservedObject var note: Note
+  
+  var body: some View {
+    if (config.showCreationTime || config.showNotificationTime) {
+      HStack {
+        if (config.showCreationTime) {
+          Image(systemName: "calendar")
+          Text("\(note.timestamp ?? Date.now, formatter: Note.dateFormatter)")
+        }
+        if (config.showNotificationTime || note.priority.isCustom()) {
+          Image(systemName: "bell")
+          Text(note.priority.getIntervalDescription())
+        }
+        Spacer()
+      }
+      .font(.footnote)
+      .foregroundColor(note.getSecondaryColor())
+      .padding(.bottom, 0.2)
+    }
+  }
 }
 
 struct OneNoteView_Previews: PreviewProvider {
-    static var previews: some View {
-        OneNoteView(config: Config(), note: Note.previewNotes.first!)
-            .previewLayout(PreviewLayout.sizeThatFits)
-            .padding()
-    }
-    
+  static var previews: some View {
+    OneNoteView(config: Config(), note: Note.previewNotes.first!)
+      .previewLayout(PreviewLayout.sizeThatFits)
+      .padding()
+  }
 }
